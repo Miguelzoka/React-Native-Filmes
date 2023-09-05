f4b30bf441fe7f041aed6e52fdb9be4a086ac8b7
Miguel Lima <miguel.pwg@gmail.com>
2023-09-04 21:59:39 -0300

    commit

---
+++ services/apiFilmes.js
@@ -0,0 +1,10 @@
+import axios from "axios";
+
+const apiFilmes = axios.create({
+    baseURL: 'https://api.themoviedb.org/3/',
+    headers: {
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzZmFkMmNkMWYzZWM0NzQ1M2ZmNjQ0NGYxYjZhNGRkOCIsInN1YiI6IjY0MzQ4YzhlZDEzMmQ2MDA3N2VkYjgwOCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.ZSh56LmhmgcjclenM7ninQHsPjR7r5CZ1c6hJMhQCT4'
+    }
+})
+
+export default apiFilmes
